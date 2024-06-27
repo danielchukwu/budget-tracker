c20313b26e81e25e3f987ff0246ef26d35ee75a1
dc <00danzy@gmail.com>
2024-06-27 01:42:21 +0100

    complete turso implementation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -14,17 +14,17 @@
 
 // if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const libsql = createClient({
-  url: process.env.TURSO_DATABASE_URL ?? '',
-  authToken: process.env.TURSO_AUTH_TOKEN ?? '',
-});
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
 
-const adapter = new PrismaLibSQL(libsql);
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
 
 
 export default prisma;
